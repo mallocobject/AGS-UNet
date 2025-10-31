fb326a0244d694328c6737e20bf2af54244655e4
malloc <13877310+malloc_heu@user.noreply.gitee.com>
2025-10-31 20:28:23 +0800

    我来不及道声晚安

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils import compute_metrics, wavelet_denoise, fft_denoise
 
-method = "wt"  # wt or fft
+method = "fft"  # wt or fft
 noisy_type = "emb"  # bw, em, ma, emb
-snr_db = -4  # -4, -2, 0, 2, 4
+snr_db = 4  # -4, -2, 0, 2, 4
 
 split_dir = "./data_split"
 with open(os.path.join(split_dir, "split_info.json"), "r") as f:
@@ -25,17 +25,15 @@ clean_signals = clean_signals.transpose(0, 2, 1)  #
 
 print(f"Number of samples: {len(indices)}")
 
-for threshold in [0.25, 0.26, 0.27, 0.28, 0.29, 0.30]:
-    print(f"Threshold: {threshold}")
-    if method == "wt":
-        denoised_signals = wavelet_denoise(noisy_signals[indices], threshold=threshold)
-    elif method == "fft":
-        denoised_signals = fft_denoise(noisy_signals[indices], threshold=threshold)
+if method == "wt":
+    denoised_signals = wavelet_denoise(noisy_signals[indices], threshold=0.074)
+elif method == "fft":
+    denoised_signals = fft_denoise(noisy_signals[indices], threshold=0.0012)
 
-    metrics = compute_metrics(
-        denoised=torch.tensor(denoised_signals),
-        clean=torch.tensor(clean_signals[indices]),
-    )
+metrics = compute_metrics(
+    denoised=torch.tensor(denoised_signals),
+    clean=torch.tensor(clean_signals[indices]),
+)
 
-    print(f"Method: {method}, Noisy Type: {noisy_type}, SNR: {snr_db} dB")
-    print(f"RMSE: {metrics['RMSE']:.4f}, SNR: {metrics['SNR']:.4f} dB")
+print(f"Method: {method}, Noisy Type: {noisy_type}, SNR: {snr_db} dB")
+print(f"RMSE: {metrics['RMSE']:.4f}, SNR: {metrics['SNR']:.4f} dB")
